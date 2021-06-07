9cb6975b7bf9182e3ac7c7621c0d11dcdbf7e893
iliyantachev <iliqn.tachev@gmail.com>
2021-06-08 00:51:12 +0300

    added maven build and tests for RepoInMemoryImpl

---
+++ Hiper-Quiz-main/src/main/java/services/Service.java
@@ -0,0 +1,17 @@
+package services;
+
+import exception.EntityAlreadyExistsException;
+import exception.EntityNotFoundException;
+import model.Identifiable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Service<K, V extends Identifiable<K>> {
+    V create(V entity) throws EntityAlreadyExistsException;
+    Optional<V> read(K id);
+    V update(V entity) throws EntityNotFoundException;
+    V delete(K id) throws EntityNotFoundException;
+    List<V> getAll();
+    Optional<V> getById(K id);
+}
